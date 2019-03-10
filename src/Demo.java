2ef71237651b0b829ef285e58cde86def284a81e
moonice <1754716900@qq.com>
2019-03-10 22:17:45 +0800

    3.10

--- src/Demo.java
+++ src/Demo.java
@@ -202,3 +202,255 @@
 //        }
 //    }
 //}
+ //import static java.lang.System.*;
+//
+//public final class Demo{
+//    public static void main(String[] args){
+//        Teacher t=new Teacher();
+//        t.setName("王明");
+//        t.work();
+//
+//        Driver d=new Driver();
+//        d.setName("小陈");
+//        d.work();
+//    }
+//}
+//
+//abstract class People{
+//    private String name;
+//    public void setName(String name){
+//        this.name=name;
+//    }
+//    public String getName(){
+//        return this.name;
+//    }
+//    public  abstract void work();
+//}
+//
+//class Teacher extends People{
+//    public void work(){
+//        out.println("我的名字叫"+this.getName()+"，我正在讲课额，请大家不要东张西望");
+//    }
+//}
+//
+//class Driver extends People{
+//    public void work(){
+//        out.println("我的名字叫："+this.getName()+",我正在开车，不能接听电话。。");
+//    }
+//}
+
+////实现接口的格式如下：
+////        修饰符 class 类名 extends 父类 implements 多个接口
+//import static java.lang.System.*;
+//
+//public class Demo{
+//    public static void main(String[] args){
+//        SataHdd sh1=new SeagateHdd();
+//        SataHdd sh2=new SeagateHdd();
+//    }
+//}
+////串行硬盘接口
+//interface SataHdd{
+//    //连接线数量
+//    public static final int CONNECT_LINE=4;
+//    //写数据
+//    public void writeData(String data);
+//    //读数据
+//    public String readData();
+//}
+//
+////维修硬盘接口
+//interface fixHdd{
+//    //维修地址
+//    String address="北京市海淀区";
+//    //开始维修
+//    boolean doFix();
+//}
+//
+////希捷硬盘(这里实现接口
+//class SeagateHdd implements SataHdd,fixHdd{
+//    //希捷硬盘读取数据
+//    public String readData(){
+//        return "数据";
+//    }
+//    //希捷硬盘写入数据
+//    public void writeData(String data){
+//        out.println("写入成功");
+//    }
+//    //维修希捷键盘
+//    public boolean doFix(){
+//        return true;
+//    }
+//}
+//
+////三星硬盘
+//class SamsunHdd implements SataHdd{
+//    //三星硬盘读取数据
+//    public String readData(){
+//        return "数据";
+//    }
+//    //三星硬盘写入数据
+//    public void writeData(String data){
+//        out.println("写入成功");
+//    }
+//}
+//
+////某劣质硬盘，不能写数据
+//abstract class XXHdd implements SataHdd{
+//    //硬盘读取数据
+//    public String readData(){
+//        return "数据";
+//    }
+//}
+//通过构造方法来使用接口
+//public class Demo{
+//    //构造一个方法来使用B类中a接口里的方法
+//    public void test1(A a){
+//        a.doSth();
+//    }
+//    public static void main(String[] args){
+//        Demo d=new Demo();
+//        //B类将自己的a接口实例化
+//        A a=new B();
+//        //那么Demo类中，可以使用B类中的a接口
+//        d.test1(a);
+//    }
+//}
+//
+//interface A {
+//    public int doSth();
+//}
+//class B implements A{
+//    public int doSth(){
+//        System.out.println("now in B");
+//        return 123;
+//    }
+//}
+//public class Demo{
+//    public static void main(String[] args){
+//        //实例化泛型
+//        Point<Integer,Integer> p1=new Point<Integer, Integer>();
+//        p1.setX(10);
+//        p1.setY(20);
+//        int x=p1.getX();
+//        int y=p1.getX();
+//        System.out.println("This point is:"+x+","+y);
+//
+//        Point<Double,String> p2=new Point<Double, String>();
+//        p2.setX(25.4);
+//        p2.setY("东京180度");
+//        double m=p2.getX();
+//        String n=p2.getY();
+//        System.out.println("This point is:"+m+","+n);
+//    }
+//}
+//
+////定义泛型
+//class Point<T1,T2>{
+//    T1 x;
+//    T2 y;
+//    public T1 getX(){
+//        return x;
+//    }
+//    public void setX(T1 x){
+//        this.x=x;
+//    }
+//    public T2 getY(){
+//        return y;
+//    }
+//    public void setY(T2 y){
+//        this.y=y;
+//    }
+//}
+
+//public class Demo{
+//    public static void main(String[] args){
+//        Point<Integer,Integer> p1=new Point<Integer, Integer>();
+//        p1.setX(10);
+//        p1.setY(20);
+//        p1.printPoint(p1.getX(),p1.getY());
+//
+//        Point<Double,String> p2=new Point<Double, String>();
+//        p2.setX(14.5);
+//        p2.setY("洗吧洗撒");
+//        p2.printPoint(p2.getX(),p2.getY());
+//
+//    }
+//
+//}
+//
+////定义泛型
+//class Point<T1,T2>{
+//    T1 x;
+//    T2 y;
+//    public T1 getX(){
+//        return x;
+//    }
+//    public void setX(T1 x){
+//        this.x=x;
+//    }
+//    public T2 getY(){
+//        return y;
+//    }
+//    public void setY(T2 y){
+//        this.y=y;
+//    }
+//    public <T1,T2> void printPoint(T1 x,T2 y){
+//        System.out.println("This point is"+x+y);
+//    }
+//}
+//public class Demo{
+//    public static void main(String[] args){
+//        InfoImp<String> obj=new InfoImp<String>("www.weixueyuan.net");
+//        System.out.println("Length Of String:"+obj.getVar().length());
+//    }
+//}
+//
+////定义泛型接口
+//interface Into<T>{
+//    public T getVar();
+//}
+//
+////实现接口
+//class InfoImp<T> implements Into<T>{
+//    private T var;
+//    //定义泛型构造方法
+//    public InfoImp(T var){
+//        this.setVar(var);
+//    }
+//    public void setVar(T var){
+//        this.var=var;
+//    }
+//    public T getVar(){
+//        return this.var;
+//    }
+//}
+
+
+public class Demo{
+    public static void main(String[] args){
+        Point p=new Point();
+        p.setX(10);
+        p.setY(20.8);
+        int x=(Integer)p.getX();
+        double y=(Double)p.getY();
+        System.out.println("This point is: "+x+" "+y);
+    }
+}
+
+class Point<T1,T2>{
+    T1 x;
+    T2 y;
+    public T1 getX(){
+        return x;
+    }
+    public void setX(T1 x){
+        this.x=x;
+    }
+    public T2 getY(){
+        return y;
+    }
+    public void setY(T2 y){
+        this.y=y;
+    }
+}
--- src/Demo2.java
+++ src/Demo2.java
@@ -1,35 +1,35 @@
-public class Demo2{
-    public static void main(String[] args){
-        Teacher t=new Teacher();
-        t.name="小布";
-        t.age=70;
-        t.school="清华大学";
-        t.subject="JAVA";
-        t.seniority=12;
-        t.say();
-        t.lecturing();
-    }
-}
-class People {
-    String name ;
-    int age;
-    int height;
-
-    void say(){
-        System.out.println("我的名字是"+name+",年龄是"+age+"身高是"+height);
-    }
-}
-
-class Teacher extends People{
-    String school;
-    String subject;
-    int seniority;
-
-    void say(){
-        System.out.println("我叫"+name+",在"+school+"教"+subject+",已经"+seniority+"年了。");
-    }
-
-    void lecturing(){
-        System.out.println("我已经"+age+"岁了，讲台上");
-    }
-}
+//public class Demo2{
+//    public static void main(String[] args){
+//        Teacher t=new Teacher();
+//        t.name="小布";
+//        t.age=70;
+//        t.school="清华大学";
+//        t.subject="JAVA";
+//        t.seniority=12;
+//        t.say();
+//        t.lecturing();
+//    }
+//}
+//class People {
+//    String name ;
+//    int age;
+//    int height;
+//
+//    void say(){
+//        System.out.println("我的名字是"+name+",年龄是"+age+"身高是"+height);
+//    }
+//}
+//
+//class Teacher extends People{
+//    String school;
+//    String subject;
+//    int seniority;
+//
+//    void say(){
+//        System.out.println("我叫"+name+",在"+school+"教"+subject+",已经"+seniority+"年了。");
+//    }
+//
+//    void lecturing(){
+//        System.out.println("我已经"+age+"岁了，讲台上");
+//    }
+//}
